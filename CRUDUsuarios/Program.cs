54040ea9407f30721e71a716e55136d833fa6791
Oscar Disla <dislaoscar@gmail.com>
2026-03-31 23:24:11 -0400

    Implemeto de crear usuario

--- CRUDUsuarios/Program.cs
+++ CRUDUsuarios/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<Usuario> usuarios = new List<Usuario>();
+    static int contadorId = 1;
+
+    static void Main()
+    {
+        CrearUsuario();
+    }
+
+    static void CrearUsuario()
+    {
+        Usuario u = new Usuario();
+        u.Id = contadorId++;
+
+        Console.Write("Nombre: ");
+        u.Nombre = Console.ReadLine();
+
+        Console.Write("Email: ");
+        u.Email = Console.ReadLine();
+
+        usuarios.Add(u);
+
+        Console.WriteLine("Usuario creado correctamente.");
+    }
+}
